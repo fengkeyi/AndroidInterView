2b8256032bf90b7aa3e028224e3649a7d5b2b90f
keyi.feng@ubtrobot.com <keyi.feng@ubtrobot.com>
2020-04-29 19:12:11 +0800

    demo all

--- app/src/main/java/com/flutter/hybrid/androidinterview/handler/HandlerDemo.java
+++ app/src/main/java/com/flutter/hybrid/androidinterview/handler/HandlerDemo.java
@@ -4,6 +4,20 @@ import android.os.AsyncTask;
 
 import java.util.logging.Handler;
 
+/***
+ *  TODO
+ *      THreadLocal:当前线程存储的数据仅能从当前线程去除
+ *      MessageQueue：具有时间优先级的消息队列，单链表
+ *      Looper：消息循环队列，看是否有新的消息到来
+ *      Handler：具体处理逻辑的地方（发送消息和处理消息）
+ *      Looper在主线程中死循环为什么没有导致界面的卡死？
+ *      导致卡死的是在Ui线程中执行耗时操作导致界面出现掉帧，甚至ANR，Looper.loop()这个操作本身不会导致这个情况。
+ *      有人可能会说，我在点击事件中设置死循环会导致界面卡死，同样都是死循环，不都一样的吗？Looper会在没有消息的时候阻塞当前线程，释放CPU资源，等到有消息到来的时候，再唤醒主线程。
+ *      App进程中是需要死循环的，如果循环结束的话，App进程就结束了。
+ *      建议阅读：
+ *      《Android中为什么主线程不会因为Looper.loop()里的死循环卡死？》
+ *      https://www.zhihu.com/question/34652589
+ */
 public class HandlerDemo {
 
     public void simple() {
--- app/src/main/java/com/flutter/hybrid/androidinterview/view/ViewDemo.java
+++ app/src/main/java/com/flutter/hybrid/androidinterview/view/ViewDemo.java
@@ -39,6 +39,31 @@ public class ViewDemo {
      *  recyclerview-》LayoutManager.recycleChildren 回收
      *  复用机制：
      *  Recycler#getViewForPosition
+     *
+     *  link:https://mp.weixin.qq.com/s?__biz=MzAxMTI4MTkwNQ==&mid=2650830500&idx=1&sn=7503d4f3821435a72233bc2fab7fcdc8&chksm=80b7a23ab7c02b2c3b0030dcd8678d17a8908d03d5d2c4489314d6b2cb2f280e0359ea5f37ad&mpshare=1&scene=1&srcid=0429NERyieM9SDUQxkIJ7nE2&sharer_sharetime=1588139003849&sharer_shareid=a2256cd305676666fdd17905ad0d52b9&key=498a3c4fcbb47b96653a1e5a5314552ab390abd2eb20d2fbead37dca2587c7fd77ef98d155a9390a547d091c19db61c6ea60504ffa1f9b0d39374e974c5039e048a57820dfe047ccbfd7994c6696c1cf&ascene=1&uin=Mjg0MzYyOTU4Mw%3D%3D&devicetype=Windows+7&version=62070158&lang=zh_CN&exportkey=Aep%2BMMjy1c2mI%2BFL2GuwOzo%3D&pass_ticket=AN3S87Tn3uPcMm8hvg4N0Yl5Cdexl2a4dJN%2FmlGgR5VUTd8Uktjb%2B%2FrT%2Bae73CAk
+     *  TODO
+     *   2.Bitmap
+     *      Bitmap的内存计算方式
+     *      在已知图片的长和宽的像素的情况下，影响内存大小的因素会有资源文件位置和像素点大小
+     *          像素点大小
+     *          常见的像素点有：ARGB_8888：4个字节;ARGB_4444、ARGB_565：2个字节
+     *          不同dpi对应存放的文件夹
+     *          ldpi    mdpi    hdpi    xhdpi   xxhdpi  xxxhdpi
+     *          120     160     160-240 240-320 320-480 480-640
+     *          比如一个一张图片的像素为180*180px，dpi(设备独立像素密度)为320，如果它仅仅存放在drawable-hdpi，则有
+     *              横向像素点 = 180 * 320/240 + 0.5f = 240 px
+     *              纵向像素点 = 180 * 320/240 + 0.5f = 240 px
+     *          如果它仅仅存放在drawable-xxhdpi，则有：
+     *              横向像素点 = 180 * 320/480 + 0.5f = 120 px
+     *              纵向像素点 = 180 * 320/480 + 0.5f = 120 px
+     *          所以，对于一张180*180px的图片，设备dpi为320，资源图片仅仅存在drawable-hdpi，像素点大小为ARGB_4444，最后生成的文件内存大小为：
+     *              内存大小 = 240 * 240 * 2 = 115200byte 约等于 112.5kb
+     *      # Bitmap的高效加载
+     *          1/获取需要的长和宽，一般获取控件的长和宽。
+     *          2/设置BitmapFactory.Options中的inJustDecodeBounds为true，可以帮助我们在不加载进内存的方式获得Bitmap的长和宽。
+     *          3/对需要的长和宽和Bitmap的长和宽进行对比，从而获得压缩比例，放入BitmapFactory.Options中的inSampleSize属性。
+     *          4/设置BitmapFactory.Options中的inJustDecodeBounds为false，将图片加载进内存，进而设置到控件中。
+     *
      */
     private RecyclerView recyclerView;
 
